3bf2b5168a14b5cb2a1acb0deea0ca436ab02e26
unknown <3D-Admin@PCG010-000.edu.htl.org>
2020-02-18 13:26:06 +0100

    Add Pickup Item

---
+++ Assets/MyGame/Scripts/Pickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            var player = collision.gameObject;
+            var wormCon = player.GetComponent<WormController>();
+            wormCon.shoot = 1000f;
+            Destroy(this.gameObject);
+        }
+    }
+}
