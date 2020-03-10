5045b06bc3aed0b959e476f0a6611254a8700cb4
Will <wilfried.gruber@hotmail.com>
2020-03-10 09:21:10 +0100

    Add Healthpack

---
+++ Assets/MyGame/Scripts/HealthPack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : MonoBehaviour
+{
+    public int healthBonus = 1;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            WormController worm = collision.gameObject.GetComponent<WormController>();
+            worm.health = worm.health + healthBonus;
+
+            Destroy(this.gameObject);
+        }
+    }
+}
